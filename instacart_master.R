829dcb343a4635e8731c0ecf1fba35ddb6d216d0
adeepikaa <adeepika@gmail.com>
2020-06-11 20:52:58 -0400

    Master file

---
+++ instacart_master.R
@@ -0,0 +1,22 @@
+
+# This file reads all the input files and joins the data as needed
+# Also has commands to understand data
+source("read_input.R")
+
+# Preliminary data exploration, however further data analysis was moved to Tableau 
+# by using CSV fine to save the dataset and load in Tableau
+source("data_explore.R")
+
+# Features are defined in this file based on all Prior Order data
+source("features.R")
+
+# This files combines all the features, creates the complete training and test datasets
+source("create_sets.R")
+
+# Training set exploration of features.
+source("features_explore.R")
+
+# Based on feature exploration important features are selected
+# Different Models are run and compared before performing on the test data
+source("models.R")
+
